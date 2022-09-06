be18f4b5d8069c0554ce3f0905867dc4823f7e07
shea-fitz <sheafitz10@gmail.com>
2022-09-05 20:13:13 -0400

    data files

---
+++ src/data/projects.js
@@ -0,0 +1,112 @@
+export const projects = [
+  {  
+    title: "Altana Atlas",
+    description: "A source of truth for global supply chain",
+    year: '2022',
+    type: 'Design – Product',
+    context: 'Client – Agency',
+    link: '',
+  },
+
+  {  
+    title: 'Playlist Prompts',
+    description: "An ongoing collection of publicly sourced playlist ideas",
+    year: '2022',
+    type: 'Web Development' + 'Design',
+    context: 'Personal',
+    link: 'http://playlistprompts.com/',
+  },
+
+
+  {  
+    title: "Flowers in Motion",
+    description: "A sporadic series of mixes",
+    year: '2022',
+    type: 'Music',
+    context: 'Personal',
+    link: 'https://soundcloud.com/more-swans/sets/flowers-in-motion',
+  },
+
+
+  {  
+    title: "Blue Flower Arts",
+    description: "Brand identity for a boutique literary agency",
+    year: '2021',
+    type: 'Design – Brand',
+    context: 'Client – Freelance',
+    link: '',
+  },
+
+  {  
+    title: "Sunny Place for Shady People",
+    description: "A music visualizer",
+    year: '2021',
+    type: 'Animation',
+    context: 'Client – Freelance',
+    link: '',
+  },
+
+  {  
+    title: "Student Visa Review",
+    description: "An online exhibition of interviews with architecture students",
+    year: '2021',
+    type: 'Animation',
+    context: 'Client – Freelance',
+    link: '',
+  },
+
+  {  
+    title: "MakerDao Comm-Dev",
+    description: "A visual identity and documentation site for a global crypto community",
+    year: '2020',
+    type: 'Design – Visual',
+    context: 'Client – Freelance',
+    link: '',
+  },
+
+  {  
+    title: "Two People in a Room Over Time",
+    description: "An series of Risograph prints exploring abstract narrative storytelling",
+    year: '2019',
+    type: 'Illustration',
+    context: 'Exhibition',
+    link: '',
+  },
+
+  {  
+    title: "Nature Magazine",
+    description: "Commissioned illustrations for a leading scientific journal",
+    year: '2019',
+    type: 'Illustration',
+    context: 'Client – Freelance',
+    link: '',
+  },
+
+  {  
+    title: "I Didn't Ask for the Habit",
+    description: "An abstract comic made with generative 3D modeling software",
+    year: '2018',
+    type: 'Illustration',
+    context: 'Personal',
+    link: '',
+  },
+
+  {  
+    title: "Digital Drawings",
+    description: "Illustration and animation experiments with generative 3D software",
+    year: '2018 - 2020',
+    type: 'Animation' + 'Illustration',
+    context: 'Personal',
+    link: '',
+  },
+
+  {  
+    title: "Website Archive",
+    description: "The evolution of my personal website",
+    year: '2018 - ?',
+    type: 'Web Development' + 'Design',
+    context: 'Personal',
+    link: '',
+  },
+
+];
---
+++ src/data/writing.js
@@ -0,0 +1,68 @@
+export const writing = [
+    {
+      title: 'Shea Fitz on sheafitz.com',
+      link:
+        'https://blog.gossipsweb.net/behind-the-scenes/shea-fitz-on-shea-fitz.html',
+        source: 'Gossips Web',
+        year: '2021'
+    },
+
+    {
+        title: 'A playlist that is a poem',
+        link:
+          "https://store.are.na/products/are-na-annual-2021#:~:text=This%20year's%20Annual%20is%20themed,fiona%20carty%2C%20Juliana%20Castro%2C%20R.C.",
+          source: 'Are.na Annual',
+          year: '2021'
+    },
+
+    // {
+    //     title: 'SFPC Alum Interview',
+    //     link:
+    //       'https://medium.com/sfpc/sfpc-alum-interview-shea-fitzpatrick-1f352222896c',
+    //       source: 'School for Poetic Computation blog',
+    //       year: '2020'
+    // },
+
+    {
+        title: 'The First Computer Mouse Conference',
+        link:
+          'https://www.are.na/blog/an-entire-conference-dedicated-to-the-computer-mouse',
+        source: 'Are.na Blog',
+        year: '2019'
+    },
+
+    {
+        title: "The Queer Zine Library that's Finding a Global Community",
+        link:
+          'https://eyeondesign.aiga.org/the-queer-zine-library-in-hong-kong-thats-finding-a-global-community/',
+        source: 'AIGA Eye on Design',
+        year: '2019'
+    },
+
+    {
+        title: "RAPAPAWN's Mind-Blending Loops",
+        link:
+          'https://eyeondesign.aiga.org/rapapawns-mind-bending-loops-blend-early-avant-garde-animation-with-abstract-comics/',
+        source: 'AIGA Eye on Design',
+        year: '2018'
+    },
+
+    {
+        title: 'Queer Technologies',
+        link:
+          'https://www.are.na/blog/queer-technologies',
+        source: 'Are.na Blog',
+        year: '2018'
+    },
+
+    {
+        title: 'Breaking the Sequence',
+        link:
+          'https://www.are.na/blog/breaking-the-sequence',
+        source: 'Are.na Blog',
+        year: '2017'
+    },
+
+
+
+  ];
