56ff5d07ef4e07da08c672ded13afe26efe39973
shea-fitz <sheafitz10@gmail.com>
2022-09-13 21:49:39 -0400

    arena api for gallery

--- src/pages/sketch.js
+++ src/pages/sketch.js
@@ -3,10 +3,21 @@ import PageHeader from '../components/PageHeader';
 import Image from '../components/Image';
 import { Link } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import {drawings} from '../data/drawings';
 
   
 const Sketch = () => {
+  const [images, setImg] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.are.na/v2/channels/website-gallery-siuqv3hfpny').then(res => {
+      setImg(res.data.contents);
+    }).catch(err => console.log(err));
+  }, []);
+
   return (
     <div className="page-wrapper">
       <Link to='/' className="back">home</Link>
@@ -18,16 +29,11 @@ const Sketch = () => {
               year='2018 - 2020'
             />
 
-
-            <div className="image-gallery">
-            {drawings.map(drawing => (
-              <div className="column">
-                <Image
-                    source={drawing.source}
-                  />
-              </div>
-                ))}     
-            </div>
+         <div className="image-gallery">
+          {images && images.map(el => {
+                return (<div className="column"><img key={el.key} src={el.image.original.url}/></div>)
+              })}  
+        </div>
 
         </div>
     </div>
