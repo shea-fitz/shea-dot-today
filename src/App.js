4654497c956f7ddeb21004422fd78b8de7fedd7a
shea-fitz <sheafitz10@gmail.com>
2023-11-30 19:15:57 -0500

    uncensor

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
     <div className="wrapper">  
       <div className="mobile-info">
-             <div className="header center">♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪</div>
+             <div className="header center">♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪♫♪</div>
         <Info/>
       </div>
 
