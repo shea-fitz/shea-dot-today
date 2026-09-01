d0bf97d011e084613d5ecad8e6250b73a004d9ce
shea-fitz <sheafitz10@gmail.com>
2026-09-01 15:40:29 -0400

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -9,7 +9,7 @@ const Info = () => {
 
 return (
 <div className="info">
-    <p className="bio">Designer who can build things, musician who can design things...currently at The New York Times.
+    <p className="bio">I design products with a brand sensibility and create narrative experiences systematically, currently at The New York Times.
     <br/><br/>
 
     </p>
